1d529f828dc5b0b518f218590b890c9d193d67fc
Tahsin Abrar <abrar.cste@gmail.com>
2014-08-15 20:26:04 +0600

    Initial commit

---
+++ app/views/hello.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="IE=edge, chrome=1">
+	<title>My Laravel Learning Website</title>
+    <meta name="viewport" content="width=device-width">
+</head>
+<body>
+    Hello from the view
+</body>
+</html>
